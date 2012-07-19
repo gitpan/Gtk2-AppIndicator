4ad70c329cf185d4e141be5b6fc3fa216cc2b04e
Hans Oesterholt <oesterhol@cpan.org>
2012-07-19 23:21:17 +0000

    Import of OESTERHOL/Gtk2-AppIndicator-0.13 from CPAN.
    gitpan-cpan-distribution: Gtk2-AppIndicator
    gitpan-cpan-version:      0.13
    gitpan-cpan-path:         OESTERHOL/Gtk2-AppIndicator-0.13.tar.gz
    gitpan-cpan-author:       OESTERHOL
    gitpan-cpan-maturity:     released

--- lib/Gtk2/AppIndicator.pm
+++ lib/Gtk2/AppIndicator.pm
@@ -26,7 +26,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw(
 );
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 #sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
@@ -60,19 +60,40 @@ XSLoader::load('Gtk2::AppIndicator', $VERSION);
 
 sub new {
   my $class=shift;
-  my $appname=shift or die "Gtk2::AppIndicator->new needs an application name";
+  my $application_id=shift or die "Gtk2::AppIndicator->new needs an application id";
   my $iconname=shift or die "Gtk2::AppIndicator->new needs an icon name";
+  my $type=shift;
+  if (not(defined($type))) { $type="application-status"; }
+  
+  my $tp=-1;
+  if ($type eq "application-status") { $tp=1; }
+  elsif ($type eq "communications") { $tp=2; }
+  elsif ($type eq "system-services") { $tp=3; }
+  elsif ($type eq "hardware") { $tp=4; }
+  elsif ($type eq "other") { $tp=5; }
+  
+  if ($tp==-1) { die "Gtk2::AppIndicator->new -> category of indicator must be one of 'application-status','communications','system-services','hardware','other'}"; }
   
   my $obj={};
   bless $obj,$class;
-  $obj->{ind}=appindicator_new($appname,$iconname);
+  $obj->{ind}=appindicator_new($application_id,$iconname,$tp);
   return $obj;
+}
+
+sub get_category {
+	my $self=shift;
+	return appindicator_get_category($self->{ind});
 }
 
 sub set_icon_theme_path {
 	my $self=shift;
 	my $path=shift;
 	appindicator_set_icon_theme_path($self->{ind},$path);
+}
+
+sub get_icon_theme_path {
+	my $self=shift;
+	return appindicator_get_icon_theme_path($self->{ind});
 }
 
 sub set_icon_name_active {
@@ -83,6 +104,23 @@ sub set_icon_name_active {
 	appindicator_set_icon_name_active($self->{ind},$name,$text);
 }
 
+sub get_icon_name_active {
+	my $self=shift;
+	return appindicator_get_icon($self->{ind});
+}
+
+sub get_icon_desc_active {
+	my $self=shift;
+	return appindicator_get_icon_desc($self->{ind});
+}
+
+sub get_id {
+	my $self=shift;
+	return appindicator_get_id($self->{ind});
+}
+
+
+
 sub set_icon_name_attention {
 	my $self=shift;
 	my $name=shift;
@@ -90,6 +128,17 @@ sub set_icon_name_attention {
 	if (not(defined($text))) { $text="no text"; }
 	appindicator_set_icon_name_attention($self->{ind},$name,$text);
 }
+
+sub get_icon_name_attention {
+	my $self=shift;
+	return appindicator_get_attention_icon($self->{ind});
+}
+
+sub get_icon_desc_attention {
+	my $self=shift;
+	return appindicator_get_attention_icon_desc($self->{ind});
+}
+
 
 sub set_active {
 	my $self=shift;
@@ -101,12 +150,80 @@ sub set_attention {
 	appindicator_set_attention($self->{ind});
 }
 
+sub set_passive {
+	my $self=shift;
+	appindicator_set_passive($self->{ind});
+}
+
+sub set_status {
+	my $self=shift;
+	my $status=shift;
+	if ($status eq "passive") { $self->set_passive(); }
+	elsif ($status eq "active") { $self->set_active(); }
+	elsif ($status eq "attention") { $self->set_attention(); }
+	else { die "usage: set_status <'passive'|'active'|'attention'>"; }
+}
+
+sub get_status {
+	my $self=shift;
+	return appindicator_get_status($self->{ind});
+}
+
 
 sub set_menu {
 	my $self=shift;
 	my $menu=shift;
+	$self->{menu}=$menu;
 	appindicator_set_menu($self->{ind},$menu);
 }
+
+sub get_menu {
+	my $self=shift;
+	return $self->{menu};
+}
+
+sub set_secondary_active_target {
+	my $self=shift;
+	my $widget=shift;
+	$self->{secondary}=$widget;
+	appindicator_set_secondary_active_target($self->{ind},$widget);
+}
+
+sub get_secondary_active_target {
+	my $self=shift;
+	return $self->{secondary};
+}
+
+sub set_title {
+	my $self=shift;
+	my $title=shift;
+	appindicator_set_title($self->{ind},$title);
+}
+
+sub get_title {
+	my $self=shift;
+	return appindicator_get_title($self->{ind});
+}
+
+sub set_label {
+	my $self=shift;
+	my $label=shift or die "usage: set_label(label,guide)";
+	my $guide=shift or die "usage: set_label(label,guide)";
+	appindicator_set_label($self->{ind},$label,$guide);
+}
+
+sub get_label {
+	my $self=shift;
+	return appindicator_get_label($self->{ind});
+}
+
+sub get_guide {
+	my $self=shift;
+	return appindicator_get_label_guide($self->{ind});
+}
+
+
+
 
 
 1;
@@ -142,21 +259,43 @@ This module gives an interface to the new ubuntu Unity libappindicator stuff.
 
 =head1 FUNCTIONS
 
- $ind=Gtk2::AppIndicator->new($application_name,$active_icon_name)
+ $ind=Gtk2::AppIndicator->new($application_id,$active_icon_name [,$category])
  
-Creates a new application indicator object with given name and icon name for the active icon.
+Creates a new application indicator object with given name (id) and icon name for the active icon.
+Category must be one of  { 'application-status','communications','system-services','hardware','other' }
+if set. if not set, it defaults to 'application-status'.
 
  $ind->set_icon_theme_path($path)
  
 Set the icon theme path to 'path'. This is where icons should be found with names like <active_icon_name>.png.
 
+ $ind->get_icon_theme_path() 
+ 
+Returns the (previously written) icon theme path, or undefined if not set.
+
+ $ind->get_category()
+ 
+Returns the previously set category with the new function.
+
+ $ind->get_id()
+ 
+Returns the application id given to the new function.
+
  $ind->set_icon_name_active($name)
  
 Sets the icon name for the active icon.
 
+ $ind->get_icon_name_active()
+ 
+Returns the name of the icon for active state.
+
  $ind->set_icon_name_attention($name)
  
 Sets the icon name for the attention icon
+
+ $ind->get_icon_name_attention()
+ 
+Returns the name of the icon for attention state.
 
   $ind->set_active()
   
@@ -166,9 +305,33 @@ Makes the application indicator active.
   
 Makes the application indicator show the attention icon.
 
+  $ind->set_passive()
+  
+Makes the application indicator enter passive state, not showing any icon
+
+  $ind->set_state($state)
+  
+Sets application indicator in the given state, one of {'active','passive','attention'}.
+
+  $ind->get_state()
+  
+Returns the current state of the application indicator.
+
   $ind->set_menu($menu)
   
 Sets the popup menu for the indicator icon.
+
+  $ind->get_menu()
+  
+Returns the current menu (not from the C code, but as stored in the perl object)
+
+  $ind->set_secondary_activate_target($widget)
+  
+Sets the secondary active target (under the middle mouse button) to $widget
+
+  $ind->get_secondary_activate_target()
+  
+Returns the current secondary active target (not from the C code, but as stored in the perl object)
 
 =head1 AUTHOR
 
